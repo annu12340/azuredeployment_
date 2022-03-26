f45d83e76876d974ea5f31610d3edc903838cf42
annu12340 <annujolly17@gmail.com>
2022-03-26 22:16:46 +0530

    req.txt

--- my_site/mysite/settings.py
+++ my_site/mysite/settings.py
@@ -116,9 +116,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STRIPE_SECRET_KEY= "STRIPE_SECRET_KEY"
