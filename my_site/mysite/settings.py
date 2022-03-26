af1235abfaf64c64a85df908eb400e0f8afe8bc0
Annu <annujolly17@gmail.com>
2022-03-26 21:17:07 +0530

    Update settings.py

--- my_site/mysite/settings.py
+++ my_site/mysite/settings.py
@@ -131,6 +131,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STRIPE_SECRET_KEY= "STRIPE_SECRET_KEY"
 STRIPE_PUBLISHABLE_KEY="STRIPE_SECRET_KEY"
