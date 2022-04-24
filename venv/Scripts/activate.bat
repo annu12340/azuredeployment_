0d95e52be09b830c406c79e637ebbc3ef375eab5
Annu <annujolly17@gmail.com>
2022-04-24 09:42:17 +0530

    minor update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\admin\Desktop\azuredeployment_hackathonproject\venv
+set VIRTUAL_ENV=C:\Users\admin\Desktop\django-ecommerce\DevTools\gitauto\azuredeployment_\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
